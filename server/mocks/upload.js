b7ea1c06324e094ee02683d72322baea6ca5dd89
rnkumar <raja.kumar@certesnetworks.com>
2016-01-02 15:44:53 +0530

    CORS

--- server/mocks/upload.js
+++ server/mocks/upload.js
@@ -1,4 +1,10 @@
 module.exports = function (app) {
+  app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
   var express = require('express');
   var uploadRouter = express.Router();
 
