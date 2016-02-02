edb6a1a35360e973949630bb156c07b4dcf64c54
rnkumar <raja.kumar@certesnetworks.com>
2016-02-02 20:51:22 +0530

    with progress 100% or not.. check and show size if ok

--- app/components/file-upload-attachments.js
+++ app/components/file-upload-attachments.js
@@ -1,14 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  files: "",
-
   didInit: function () {
-    var cRef = this;
-    Ember.Logger.info("try",cRef.get("dropzoneRef"));
-    //Ember.set("files",  Ember.computed(function () {
-    //  Ember.Logger.info(cRef);
-    //  return cRef.files;
-    //}));
   }.on('didInsertElement')
 });
